3fe60d5b08cf060f980e0625b25b82b0f32a6276
srivatsan <vatsank@gmail.com>
2021-01-19 13:01:15 +0530

    Update

--- tour-rest-api/src/main/java/com/example/demo/controllers/TourController.java
+++ tour-rest-api/src/main/java/com/example/demo/controllers/TourController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,10 +44,11 @@ public class TourController {
 
 	
 	@PostMapping(path = "/")
-	public Tour addTour(@RequestBody Tour tour) {
+	public ResponseEntity<Tour> addTour(@RequestBody Tour tour) {
 		
-		 return this.service.addTour(tour);
 		
+		 return ResponseEntity.status(HttpStatus.CREATED).body(this.service.addTour(tour));
+					
 	}
 	
 }
