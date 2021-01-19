14aba9469e48d99e6f1ff23535772919d21182a8
srivatsan <vatsank@gmail.com>
2021-01-19 12:46:11 +0530

    Update

--- tour-rest-api/src/main/java/com/example/demo/TourRestApiApplication.java
+++ tour-rest-api/src/main/java/com/example/demo/TourRestApiApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.Tour;
+import com.example.demo.repos.TourRepository;
 
 @SpringBootApplication
 public class TourRestApiApplication {
@@ -10,4 +16,23 @@ public class TourRestApiApplication {
 		SpringApplication.run(TourRestApiApplication.class, args);
 	}
 
+	
+	@Bean
+	public CommandLineRunner runner() {
+		
+		
+		return new CommandLineRunner() {
+			
+			@Autowired
+			TourRepository repo;
+			@Override
+			public void run(String... args) throws Exception {
+				
+				
+				repo.save(new Tour(101,"Simla",56000));
+				repo.save(new Tour(102,"Jammu",76000));
+				
+			}
+		};
+	}
 }
