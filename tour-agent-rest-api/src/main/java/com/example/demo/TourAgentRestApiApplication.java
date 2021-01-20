e52fde0118913203ac69180c503994912e4f6cb2
srivatsan <vatsank@gmail.com>
2021-01-20 16:47:19 +0530

    Update

--- tour-agent-rest-api/src/main/java/com/example/demo/TourAgentRestApiApplication.java
+++ tour-agent-rest-api/src/main/java/com/example/demo/TourAgentRestApiApplication.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.example.demo.entity.TourAgent;
 import com.example.demo.repos.TourAgentRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TourAgentRestApiApplication {
 
 	public static void main(String[] args) {
