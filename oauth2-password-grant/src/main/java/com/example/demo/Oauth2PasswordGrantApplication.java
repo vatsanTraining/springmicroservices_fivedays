c83a599b513793c2e6f3ccf023474dc25762e5d3
srivatsan <vatsank@gmail.com>
2021-01-22 13:02:50 +0530

    Update

--- oauth2-password-grant/src/main/java/com/example/demo/Oauth2PasswordGrantApplication.java
+++ oauth2-password-grant/src/main/java/com/example/demo/Oauth2PasswordGrantApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import com.example.demo.model.Hospital;
@@ -27,5 +29,11 @@ public class Oauth2PasswordGrantApplication {
 	public Hospital sahayadri() {
 		
 		return new Hospital(2020, "Sahayadri", 780000100);
+	}
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		
+		return new BCryptPasswordEncoder();
 	}
 }
