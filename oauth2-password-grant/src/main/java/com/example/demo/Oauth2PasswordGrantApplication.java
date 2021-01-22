42a99947aad0bc2bef6730a1a4218aea00fa2015
srivatsan <vatsank@gmail.com>
2021-01-22 12:27:03 +0530

    Update

--- oauth2-password-grant/src/main/java/com/example/demo/Oauth2PasswordGrantApplication.java
+++ oauth2-password-grant/src/main/java/com/example/demo/Oauth2PasswordGrantApplication.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import com.example.demo.model.Hospital;
 
 @SpringBootApplication
+@EnableResourceServer
 public class Oauth2PasswordGrantApplication {
 
 	public static void main(String[] args) {
--- oauth2-password-grant/src/main/java/com/example/demo/controllers/HospitalController.java
+++ oauth2-password-grant/src/main/java/com/example/demo/controllers/HospitalController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,11 @@ public class HospitalController {
 
 	
 	@Autowired
+	@Qualifier("fortis")
 	private Hospital fortis;
 	
 	@Autowired
+	@Qualifier("sahayadri")
 	private  Hospital sahya;
 	
 	
